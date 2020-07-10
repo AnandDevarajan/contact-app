ededbb07a06db63206c844a5e3cac8b2e75a34a1
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-10 11:18:23 +0530

    added basic get route

--- Backend/server.js
+++ Backend/server.js
@@ -3,4 +3,10 @@ const app = express();
 
 const port = process.env.port || 5000;
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to my app',
+  });
+});
+
 app.listen(port, () => console.log(`App running on port ${port}`));
