60b3c7cc67406fdf2e6f05abbdda3d3034376ff9
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-10 12:37:47 +0530

    Added user to DB

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../model/User');
@@ -15,14 +16,34 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({
         errors: errors.array(),
       });
     }
-    res.send('Success');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({
+          message: 'email already exits',
+        });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send('User saved to DB');
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
