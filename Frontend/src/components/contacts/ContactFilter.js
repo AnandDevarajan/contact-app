aa1177e67624078f3f0c5ce3b4acd830c7d2a4f1
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-11 20:17:46 +0530

    filter component added

---
+++ Frontend/src/components/contacts/ContactFilter.js
@@ -0,0 +1,28 @@
+import React, { useContext, useRef, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+const ContactFilter = () => {
+  const contactContext = useContext(ContactContext);
+  const { filterContact, clearFilter, filtered } = contactContext;
+  const text = useRef('');
+
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = '';
+    }
+  }, []);
+
+  const onChange = (e) => {
+    if (text.current.value !== '') {
+      filterContact(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+  return (
+    <form>
+      <input ref={text} type='text' placeHolder='search' onChange={onChange} />
+    </form>
+  );
+};
+
+export default ContactFilter;
