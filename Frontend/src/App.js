fdeebc168012624ea7d79d1b971779876efcb834
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-11 16:55:52 +0530

    added badges to type

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
--- Frontend/src/components/contacts/ContactItem.js
+++ Frontend/src/components/contacts/ContactItem.js
@@ -1,20 +1,13 @@
 import React from 'react';
+import { Badge, BadgeProps } from 'reactstrap';
 
 const ContactItem = ({ contact }) => {
   const { id, name, email, phone, type } = contact;
   return (
     <div className='card bg-light'>
       <h3 className='text-left'>
-        {name}
-        {' '}
-        <span
-          className={
-            'badge' +
-            (type === 'professional' ? 'badge-success' : 'badge-danger')
-          }
-        >
-          {type}
-        </span>
+        {name}{' '}
+        <Badge color={type === 'personal' ? 'success' : 'dark'}>{type}</Badge>
       </h3>
     </div>
   );
