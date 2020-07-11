462f4c36ed789b41356afff508e4750c0255f40b
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-11 19:27:05 +0530

    useEffect added

--- Frontend/src/components/contacts/ContactForm.js
+++ Frontend/src/components/contacts/ContactForm.js
@@ -1,9 +1,23 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
   const contactContext = useContext(ContactContext);
-  const { addContact } = contactContext;
+  const { addContact, current } = contactContext;
+
+  useEffect(() => {
+    if (current != null) {
+      setContact(current);
+    } else {
+      setContact({
+        name: '',
+        email: '',
+        phone: '',
+        type: 'personal',
+      });
+    }
+  }, [contactContext, current]);
+
   const [contact, setContact] = useState({
     name: '',
     email: '',
