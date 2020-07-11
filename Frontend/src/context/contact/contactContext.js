f6ef4226b9b14b25f541caf6d0f3234674c2cc5f
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-11 11:49:10 +0530

    contact context created

--- Frontend/src/context/contact/contactContext.js
+++ Frontend/src/context/contact/contactContext.js
@@ -1,4 +1,5 @@
-import {createContext} from "react"
+import { createContext } from 'react';
 
-const contactContext=createContext();
+const contactContext = createContext();
 
+export default contactContext;
