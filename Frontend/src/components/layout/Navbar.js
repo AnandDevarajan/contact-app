75f88e92bf5fb7c00d6661d4547493d17de47aa7
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-12 18:05:15 +0530

    bug

--- Frontend/src/components/layout/Navbar.js
+++ Frontend/src/components/layout/Navbar.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
+import AuthContext from '../../context/auth/authContext';
 import { Link } from 'react-router-dom';
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a href='#!'>
+          <i className='fa fa-sign-out'></i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/signup' style={{ color: '#50d890', textDecoration: 'none' }}>
+          Sign Up
+        </Link>
+      </li>
+      <li>
+        <Link to='/login' style={{ color: '#50d890', textDecoration: 'none' }}>
+          Login
+        </Link>
+      </li>
+    </Fragment>
+  );
   return (
     <div
       className='navbar'
@@ -10,28 +41,7 @@ const Navbar = ({ title, icon }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
-      <ul>
-        <li>
-          <Link to='/' style={{ color: '#50d890',textDecoration:"none" }}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to='/about' style={{ color: '#50d890',textDecoration:"none" }}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to='/signup' style={{ color: '#50d890' ,textDecoration:"none"}}>
-            Sign Up
-          </Link>
-        </li>
-        <li>
-          <Link to='/login' style={{ color: '#50d890',textDecoration:"none" }}>
-            Login
-          </Link>
-        </li>
-      </ul>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
 };
